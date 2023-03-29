1613bfcd8def31208b3dd19362827f239f6f2ea5
Manu <emanuelecanolla@hotmail.com>
2023-03-29 14:51:04 -0300

    ajustes branch

--- dnc-e-com-grupo/src/views/Login/Login.jsx
+++ dnc-e-com-grupo/src/views/Login/Login.jsx
@@ -1,18 +1,53 @@
-import './index.scss'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Header from "../../components/Header/Header"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Header from "../../components/Header/Header";
+import './index.scss';
 
 const Login = () => {
-  return (
-    <div>
-      <Header />
-      <h2>Login html aqui</h2>
-      <button type="submit">
-            <Link to={"/home"} >Entrar</Link>
-      </button>
-    </div>
-  )
-}
+  const [cpf, setCpf] = useState('');
+  const [pass, setPass] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-export default Login
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (cpf.trim() === '' || pass.trim() === '') {
+      setErrorMessage('Por favor, preencha todos os campos.');
+    } else {
+      return true;
+    }
+  };
+
+  const handleCpfChange = (event) => {
+    setCpf(event.target.value);
+    if (event.target.value.trim() !== '' && pass.trim() !== '') {
+      setErrorMessage('');
+    }
+  };
+
+  const handlePassChange = (event) => {
+    setPass(event.target.value);
+    if (event.target.value.trim() !== '' && cpf.trim() !== '') {
+      setErrorMessage('');
+    }
+  };
+
+  return (
+    <div className="login">
+      <Header />
+      <form onSubmit={handleSubmit}>
+        <label>
+          Digite seu CPF:
+          <input type="text" placeholder="CPF" name="cpf" value={cpf} onChange={handleCpfChange} />
+        </label>
+        <label>
+          Senha:
+          <input type="password" name="pass" placeholder="*****" value={pass} onChange={handlePassChange} />
+        </label>
+        <button type="submit">Entrar</button>
+      </form>
+      {errorMessage && <div className="error">{errorMessage}</div>}
+    </div>
+  );
+};
+
+export default Login;
