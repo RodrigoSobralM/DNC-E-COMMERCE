e71e1f14aeac1467fab59d6f9de18c01d26f059e
rodrigmeira <meiramuisc@outlook.com>
2023-03-31 18:11:45 -0300

    Modal de atendimento estilizado

--- dnc-e-com-grupo/src/views/Checkout/Checkout.jsx
+++ dnc-e-com-grupo/src/views/Checkout/Checkout.jsx
@@ -4,15 +4,29 @@ import { Link } from 'react-router-dom'
 import HeaderSearchModal from '../../components/HeaderSearchModal/HeaderSearchModal'
 import Footer from '../../components/Footer/Footer'
 
-const Checkout = () => {
+const Checkout = (data) => {
   return (
     <div>
       <HeaderSearchModal />
       <div>
-        <h2>Checkout html aqui</h2>
-        <button type="submit">
-              <Link to={"/home"} >Send</Link>
-        </button>
+        <h2>Finalizar compra</h2>
+        <form>
+          <label htmlFor="name">Digite seu nome:</label>
+          <input type="text" placeholder='Nome Completo' id="name" name="name" />
+
+          <label htmlFor="cpf">Digite seu CPF:</label>
+          <input type="number" placeholder='CPF' id="cpf" name="cpf" />
+
+          <label htmlFor="address">Endereço:</label>
+          <input type="text" placeholder='Seu endereço' id="address" name="address" />
+
+          <label htmlFor="payment">Forma de pagamento:</label>
+          <input type="text" placeholder='********' id="payment" name="payment" />
+
+          <button type="submit">
+            <Link to={"/home"}>Enviar</Link>
+          </button>
+        </form>
       </div>
       <Footer />
     </div>
