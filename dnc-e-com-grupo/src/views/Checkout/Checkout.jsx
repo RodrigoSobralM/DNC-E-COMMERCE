2097c870484d3ed22b05f2fdf4155fba565e4305
fezinho <opugnero@gmail.com>
2023-04-03 11:21:09 -0300

    checkout style

--- dnc-e-com-grupo/src/views/Checkout/Checkout.jsx
+++ dnc-e-com-grupo/src/views/Checkout/Checkout.jsx
@@ -4,30 +4,33 @@ import { Link } from 'react-router-dom'
 import HeaderSearchModal from '../../components/HeaderSearchModal/HeaderSearchModal'
 import Footer from '../../components/Footer/Footer'
 
-const Checkout = (data) => {
+const Checkout = () => {
   return (
     <div>
       <HeaderSearchModal />
-      <div>
-        <h2>Finalizar compra</h2>
-        <form>
-          <label htmlFor="name">Digite seu nome:</label>
-          <input type="text" placeholder='Nome Completo' id="name" name="name" />
+      <section className="product-detail-modal">
+        <div className="product-detail-modal__container">
+          <h2>Finalizar compra</h2>
+          <form>
+            <label htmlFor="name">Digite seu nome:</label>
+            <input type="text" placeholder='Nome Completo' id="name" name="name" />
 
-          <label htmlFor="cpf">Digite seu CPF:</label>
-          <input type="number" placeholder='CPF' id="cpf" name="cpf" />
+            <label htmlFor="cpf">Digite seu CPF:</label>
+            <input type="text" placeholder='CPF' id="cpf" name="cpf" />
 
-          <label htmlFor="address">Endereço:</label>
-          <input type="text" placeholder='Seu endereço' id="address" name="address" />
+            <label htmlFor="address">Endereço:</label>
+            <input type="text" placeholder='Seu endereço' id="address" name="address" />
 
-          <label htmlFor="payment">Forma de pagamento:</label>
-          <input type="text" placeholder='********' id="payment" name="payment" />
-
-          <button type="submit">
-            <Link to={"/home"}>Enviar</Link>
-          </button>
-        </form>
-      </div>
+            <label htmlFor="payment">Forma de pagamento:</label>
+            <input type="text" placeholder='********' id="payment" name="payment" />
+            <div className="button">
+              <button type="submit">
+                <Link to={"/home"}>Enviar</Link>
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
       <Footer />
     </div>
   )
