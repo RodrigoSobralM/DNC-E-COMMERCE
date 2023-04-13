0d39b96e51a275e1fdaef2741554d6ad80ba45d1
dopezinho <opugnero@gmail.com>
2023-04-13 10:07:57 -0300

    removed colors from mock.js

--- dnc-e-com-grupo/src/mock/mock.js
+++ dnc-e-com-grupo/src/mock/mock.js
@@ -1,50 +1,50 @@
 export const PRODUCTS_MOCK = [
-    {
-        id: 1,
-        imgPath: "/image 21.png",
-        imgPathDetail: "O Xbox One X preto é um console de videogame da Microsoft com cor predominante preta e acabamento fosco. É poderoso e capaz de oferecer jogos em 4K e HDR. Vem com um controlador sem fio preto e uma variedade de portas para conectar dispositivos externos.",
-        title: "Microsoft Xbox Series X | Preto",
-        price: "R$ 2.000,00",
-        colors: ["black"],
-    },
-    {
-        id: 2,
-        imgPath: "/image 22.png",
-        imgPathDetail: "O Xbox Series S branco é um console de videogame da Microsoft em cor branca, menor e mais acessível que o Xbox Series X. É projetado para jogos em 1080p e 1440p, oferece armazenamento interno de 512GB em estado sólido, suporte a Ray Tracing e vem com um controlador sem fio branco.",
-        title: "Microsoft Xbox Series S | Branco",
-        price: "R$ ??",
-        colors: ["white"],
-        },
-        {
-        id: 3,
-        imgPath: "/image 23.png",
-        imgPathDetail: "O Nintendo Switch é um console de videogame híbrido que pode ser usado como um console de mesa ou um dispositivo portátil. Ele vem com um jogo incluído e oferece uma ampla seleção de jogos exclusivos da Nintendo.",
-        title: "Nintendo Switch com Jogo",
-        price: "R$ ??",
-        colors: ["??"],
-        },
-        {
-        id: 4,
-        imgPath: "/image 24.png",
-        imgPathDetail: "God of War é um jogo de ação e aventura para PlayStation 4 com Kratos como protagonista, acompanhado de seu filho Atreus. O objetivo é espalhar as cinzas da mãe de Atreus em um local sagrado. O jogo apresenta gráficos impressionantes, jogabilidade desafiadora e combates intensos.",
-        title: "Jogo God of War Playstation",
-        price: "R$ ??",
-        colors: ["??"],
-        },
-        {
-        id: 5,
-        imgPath: "/image 25.png",
-        imgPathDetail: "Uncharted: The Nathan Drake Collection é uma coleção remasterizada dos três primeiros jogos da série Uncharted para PlayStation 4. O jogo segue as aventuras do caçador de tesouros Nathan Drake, com gráficos aprimorados e jogabilidade renovada. É um jogo icônico da plataforma PlayStation.",
-        title: "Jogo Uncharted: The Nathan Drake Collection",
-        price: "R$ ??",
-        colors: ["??"],
-        },
-        {
-        id: 6,
-        imgPath: "/image 26.png",
-        imgPathDetail: "Gran Turismo é uma série de jogos de corrida realista desenvolvida pela Polyphony Digital para os consoles da Sony. Os jogos apresentam uma grande variedade de carros e pistas, com o objetivo de proporcionar uma experiência de corrida autêntica e popular entre fãs de carros.",
-        title: "Jogo Gran Turismo",
-        price: "R$ ??",
-        colors: ["??"],
-        },
-]
+  {
+    id: 1,
+    imgPath: "/image 21.png",
+    imgPathDetail:
+      "O Xbox One X preto é um console de videogame da Microsoft com cor predominante preta e acabamento fosco. É poderoso e capaz de oferecer jogos em 4K e HDR. Vem com um controlador sem fio preto e uma variedade de portas para conectar dispositivos externos.",
+    title: "Microsoft Xbox Series X | Preto",
+    price: "R$ 2.000,00",
+  },
+  {
+    id: 2,
+    imgPath: "/image 22.png",
+    imgPathDetail:
+      "O Xbox Series S branco é um console de videogame da Microsoft em cor branca, menor e mais acessível que o Xbox Series X. É projetado para jogos em 1080p e 1440p, oferece armazenamento interno de 512GB em estado sólido, suporte a Ray Tracing e vem com um controlador sem fio branco.",
+    title: "Microsoft Xbox Series S | Branco",
+    price: "R$ ??",
+  },
+  {
+    id: 3,
+    imgPath: "/image 23.png",
+    imgPathDetail:
+      "O Nintendo Switch é um console de videogame híbrido que pode ser usado como um console de mesa ou um dispositivo portátil. Ele vem com um jogo incluído e oferece uma ampla seleção de jogos exclusivos da Nintendo.",
+    title: "Nintendo Switch com Jogo",
+    price: "R$ ??",
+  },
+  {
+    id: 4,
+    imgPath: "/image 24.png",
+    imgPathDetail:
+      "God of War é um jogo de ação e aventura para PlayStation 4 com Kratos como protagonista, acompanhado de seu filho Atreus. O objetivo é espalhar as cinzas da mãe de Atreus em um local sagrado. O jogo apresenta gráficos impressionantes, jogabilidade desafiadora e combates intensos.",
+    title: "Jogo God of War Playstation",
+    price: "R$ ??",
+  },
+  {
+    id: 5,
+    imgPath: "/image 25.png",
+    imgPathDetail:
+      "Uncharted: The Nathan Drake Collection é uma coleção remasterizada dos três primeiros jogos da série Uncharted para PlayStation 4. O jogo segue as aventuras do caçador de tesouros Nathan Drake, com gráficos aprimorados e jogabilidade renovada. É um jogo icônico da plataforma PlayStation.",
+    title: "Jogo Uncharted: The Nathan Drake Collection",
+    price: "R$ ??",
+  },
+  {
+    id: 6,
+    imgPath: "/image 26.png",
+    imgPathDetail:
+      "Gran Turismo é uma série de jogos de corrida realista desenvolvida pela Polyphony Digital para os consoles da Sony. Os jogos apresentam uma grande variedade de carros e pistas, com o objetivo de proporcionar uma experiência de corrida autêntica e popular entre fãs de carros.",
+    title: "Jogo Gran Turismo",
+    price: "R$ ??",
+  },
+];
