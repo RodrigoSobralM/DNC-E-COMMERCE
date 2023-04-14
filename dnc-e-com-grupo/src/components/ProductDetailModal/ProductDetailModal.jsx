f9106739a844470ff5ea0834a5cf68bf7ef63407
RodrigoSobralM <rodrigo_sobral.m@hotmail.com>
2023-04-14 12:16:38 -0300

    adicionando cep

--- dnc-e-com-grupo/src/components/ProductDetailModal/ProductDetailModal.jsx
+++ dnc-e-com-grupo/src/components/ProductDetailModal/ProductDetailModal.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 
 const ProductDetailModal = ({ data }) => {
+  const [cep, setCep] = useState("");
+
+  function handleCepChange(event) {
+    let value = event.target.value.replace(/\D/g, "");
+    value = value.replace(/^(\d{5})(\d)/, "$1-$2");
+    setCep(value);
+  }
+
   return (
     <div className="product-detail-modal">
       <div className="product-detail-modal__container">
         <div className="product-detail-modal__left-side">
           <div className="product-detail__card">
             <img src={data.imgPath} alt={data.title} />
+          </div>
+          <div className="product-detail-modal__frete">
+            <input type="text" value={cep} onChange={handleCepChange} placeholder="Insira seu CEP" />
+            <button className="btnFrete">Calcular Frete</button>
           </div>
         </div>
         <div className="product-detail-modal__right-side">
